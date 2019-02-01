cd3a54206a9858ebe38cc672347dbbfa1fecdb55
root <root@Host000c2920d3e8.connectify>
2019-02-01 07:21:41 -0800

    Added config file

--- SendSMS.php
+++ SendSMS.php
@@ -2,14 +2,19 @@
 	require_once 'vendor/autoload.php';
 	use Twilio\Rest\Client;
 
-function SendSMS($TO, $BODY, $FROM = '+19712735759')
+function SendSMS($TO, $BODY, $FROM = false)
 {
 	// Update the path below to your autoload.php,
 	// see https://getcomposer.org/doc/01-basic-usage.md
 
 	// Your Account Sid and Auth Token from twilio.com/console
-	$sid    = "ACddb824e29bc5030f15a32fc8dac5cd10";
-	$token  = "473dbc7ea53bc14a384a76c97fe73eb5";
+	$ini = parse_ini_file('config.ini');
+	$sid    = $ini['SID'];
+	$token  = $ini['Token'];
+	if(!$FROM)
+	{
+		$FROM = $ini['From'];
+	}
 	$twilio = new Client($sid, $token);
 
 
